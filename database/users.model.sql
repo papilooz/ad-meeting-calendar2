3f0330fed284f3ac39927faf6cc8ad3b19812771
papilooz <manriquequervie@gmail.com>
2025-07-05 20:44:18 +0800

    refactor(database): rename users.mode.sql

--- database/users.model.sql
+++ database/users.model.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(150) NOT NULL,
     password TEXT NOT NULL,
-    email VARCHAR(150) UNIQUE,
     role VARCHAR(50),
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
