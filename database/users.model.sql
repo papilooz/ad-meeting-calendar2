d3e674b5a9368e030b4aad097d84a780c85e761b
papilooz <manriquequervie@gmail.com>
2025-07-15 18:57:29 +0800

    refactor(users.model.sql): update table

--- database/users.model.sql
+++ database/users.model.sql
@@ -1,10 +1,11 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS public."users" (
     id SERIAL PRIMARY KEY,
     username VARCHAR(100) NOT NULL UNIQUE,
     email VARCHAR(150) NOT NULL,
     password TEXT NOT NULL,
     role VARCHAR(50),
     first_name VARCHAR(100),
+    middle_name VARCHAR(100),
     last_name VARCHAR(100),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
