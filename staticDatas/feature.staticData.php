586ec1ca71b2443aa9cceab7484632c4e39e991a
papilooz <manriquequervie@gmail.com>
2025-07-15 13:24:51 +0800

    feat(feature.static.data): add feature static

---
+++ staticDatas/feature.staticData.php
@@ -0,0 +1,19 @@
+<?php
+$featuresList = [
+    [
+        "title" => "Add Meetings",
+        "description" => "Quickly create a meeting with a title, date, time, and description.",
+        "image" => "https://www.lingfordconsulting.com.au/wp-content/uploads/2023/03/iStock-1370516097-1.jpg",
+    ],
+    [
+        "title" => "View Calendar",
+        "description" => "See all your upcoming meetings in a simple calendar or list view.",
+        "image" => "https://miro.medium.com/v2/resize:fit:1400/0*Hbxo4tmcmFL7_RK7",
+    ],
+    [
+        "title" => "Edit or Delete",
+        "description" => "Easily update meeting details or remove meetings you no longer need.",
+        "image" => "https://cdn.prod.website-files.com/63b042656c21e611f6f8be44/67f7ecf94600840b5b51e1f4_63bc2cc3976847598d45b7e8_79787c26-ff16-4c9d-a892-3dd75d66ea46.webp",
+    ],
+];
+?>
