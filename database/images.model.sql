d03f03bd357dcb7b48c05efcea64d0c0d8e3b98d
papilooz <manriquequervie@gmail.com>
2025-07-15 22:18:35 +0800

    refactor(images.model.sql): update images.model.sql

--- database/images.model.sql
+++ database/images.model.sql
@@ -1,13 +1,12 @@
 CREATE TABLE IF NOT EXISTS public."images" (
-  id               uuid        PRIMARY KEY DEFAULT gen_random_uuid(),
-  user_id          uuid        NOT NULL REFERENCES public."users"(id) ON DELETE CASCADE,
-  filename         varchar(255) NOT NULL,
-  filepath         varchar(500) NOT NULL,
-  mimetype         varchar(50)  NOT NULL,
-  size_bytes       int          NOT NULL,
-  type             varchar(50)  NOT NULL,
-  created_at       timestamptz  NOT NULL DEFAULT now()
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES public."users"(id) ON DELETE CASCADE,
+    filename VARCHAR(255) NOT NULL,
+    filepath VARCHAR(500) NOT NULL,
+    mimetype VARCHAR(50) NOT NULL,
+    size_bytes INT,
+    type VARCHAR(50),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
-
-ALTER TABLE public."users"
-  ADD COLUMN profile_image_id uuid NULL REFERENCES public."images"(id);
+ALTER TABLE public.users
+ADD COLUMN IF NOT EXISTS profile_image_id INT REFERENCES public.images(id) ON DELETE SET NULL;
